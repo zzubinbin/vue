498fa6363c3e4143317a4456c3c86c22ea2c6453
wanghuibin <wanghuibin_zb@cmos.chinamobile.com>
2019-05-07 10:44:54 +0800

    饿了么

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sell
 
-> A Vue.js project
+> sell app
 
 ## Build Setup
 
@@ -14,9 +14,6 @@ npm run dev
 # build for production with minification
 npm run build
 
-# build for production and view the bundle analyzer report
-npm run build --report
-
 # run unit tests
 npm run unit
 
@@ -27,4 +24,4 @@ npm run e2e
 npm test
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/main.js
+++ src/main.js
@@ -1,15 +1,33 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+	linkActiveClass: 'active'
+});
+
+router.map({
+	'/goods': {
+		component: goods
+	},
+	'/ratings': {
+		component: ratings
+	},
+	'seller': {
+		component: seller
+	}
+});
+
+router.start(app, '#app');
+// router.go('/goods');
