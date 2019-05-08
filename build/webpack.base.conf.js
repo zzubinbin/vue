8d47816550c5987294c15deee528e220519c31d8
wanghuibin <wanghuibin_zb@cmos.chinamobile.com>
2019-05-08 18:51:07 +0800

    2019/5/8

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,6 +74,14 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.sass$/,
+        loaders:['style','css','sass']
+      },
+      {
+        test: /\.less$/,
+        loaders:['style','css','less']
       }
     ]
   },
---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    我是 header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
