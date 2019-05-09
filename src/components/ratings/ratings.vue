8860dedb055d6c0518f208713fac156ae407985c
huibin <iehuibinzzu@163.com>
2019-05-09 08:24:12 +0800

    2019/0509 组件

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div> name: "ratings"</div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 
+
+Vue.use(router)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+let app = Vue.extend(App)
+
+let router = new router({
+  linkActiveClass: 'active'
 })
+
+//路由map方法
+router.map({
+  '/goods': {
+    component: goods
+  },
+  'ratings':{
+    component: ratings
+  },
+  'seller':{
+    component: seller
+  }
+})
+
+//路由绑定
+router.start(app,'#app')
+//设置默认跳转地址
+router.go('/goods')
