0454332b1965799602110294c9701d3373445bf3
huibin <iehuibinzzu@163.com>
2019-08-15 21:15:18 +0800

    添加koa

--- server/index.js
+++ server/index.js
@@ -7,3 +7,61 @@ app.use(async (ctx) => {
 
 app.listen(3000)
 console.log('[demo] start at port 3000 use node index.js')
+
+// let p = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log('执行完成Promise')
+//     resolve()
+//   }, 2000)
+// })
+
+// function promiseClick () {
+//   let p = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       var num = Math.ceil(Math.random() * 20)
+//       // 生成1-10的随机数
+//       console.log('随机数生成的值：', num)
+//       if (num <= 10) {
+//         resolve(num)
+//       } else {
+//         reject('数字太于10了即将执行失败回调')
+//       }
+//     }, 2000)
+//   })
+//   return p
+// }
+//
+// promiseClick().then(
+//   function (data) {
+//     console.log('resolved成功回调')
+//     console.log('成功回调接受的值：', data)
+//   },
+//   function (reason, data) {
+//     console.log('rejected失败回调')
+//     console.log('失败执行回调抛出失败原因：', reason)
+//   }
+// )
+
+function promiseClick () {
+  let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      var num = Math.ceil(Math.random() * 20)
+      console.log('随机数', num)
+      if (num <= 10) {
+        resolve(num)
+      } else {
+        reject('数字大于10即将执行失败回调')
+      }
+    }, 2000)
+  })
+  return p
+}
+
+promiseClick().then((data) => {
+  console.log('resolved成功回调')
+  console.log('成功回调接收的值：', data)
+  console.log(hui)
+}).catch((reason, data) => {
+  console.log('catch到rejected失败回调')
+  console.log('catch失败执行回调抛出失败原因:', reason)
+})
