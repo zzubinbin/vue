3f199f7052c989e15b95a36044616a4f224f76f0
huibin <iehuibinzzu@163.com>
2019-08-15 22:32:17 +0800

    study koa post

--- server/index.js
+++ server/index.js
@@ -2,11 +2,27 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx) => {
-  ctx.body = 'hello koa'
+  let url = ctx.url
+  // 从request中获取GET请求
+  let request = ctx.request
+  let reqQuery = request.query
+  let reqQuerystring = request.querystring
+
+  // 从上下文中直接获取
+  let ctxQuery = ctx.query
+  let ctxQuerystring = ctx.querystring
+  ctx.body = {
+    url,
+    reqQuery,
+    reqQuerystring,
+    ctxQuery,
+    ctxQuerystring
+  }
 })
 
-app.listen(3000)
-console.log('[demo] start at port 3000 use node index.js')
+app.listen(3000, () => {
+  console.log('[demo] start at port 3000 use node index.js')
+})
 
 // let p = new Promise(function (resolve, reject) {
 //   setTimeout(() => {
@@ -42,69 +58,69 @@ console.log('[demo] start at port 3000 use node index.js')
 //   }
 // )
 
-function promiseClick() {
-  let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      var num = Math.ceil(Math.random() * 20)
-      console.log('随机数', num)
-      if (num <= 10) {
-        resolve(num)
-      } else {
-        reject('数字大于10即将执行失败回调')
-      }
-    }, 2000)
-  })
-  return p
-}
-
-function promiseClicks() {
-  let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      var num = Math.ceil(Math.random() * 20)
-      console.log('随机数', num)
-      if (num <= 10) {
-        resolve(num)
-      } else {
-        reject('数字大于10即将执行失败回调')
-      }
-    }, 2000)
-  })
-  return p
-}
-
-function promiseClickss() {
-  let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      var num = Math.ceil(Math.random() * 20)
-      console.log('随机数', num)
-      if (num <= 10) {
-        resolve(num)
-      } else {
-        reject('数字大于10即将执行失败回调')
-      }
-    }, 2000)
-  })
-  return p
-}
-
-promiseClick().then((data) => {
-  console.log('resolved成功回调')
-  console.log('成功回调接收的值：', data)
-  console.log(hui)
-}).catch((reason, data) => {
-  console.log('catch到rejected失败回调')
-  console.log('catch失败执行回调抛出失败原因:', reason)
-})
+// function promiseClick () {
+//   let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       var num = Math.ceil(Math.random() * 20)
+//       console.log('随机数', num)
+//       if (num <= 10) {
+//         resolve(num)
+//       } else {
+//         reject ('数字大于10即将执行失败回调')
+//       }
+//     }, 2000)
+//   })
+//   return p
+// }
+//
+// function promiseClicks () {
+//   let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       var num = Math.ceil(Math.random() * 20)
+//       console.log('随机数', num)
+//       if (num <= 10) {
+//         resolve(num)
+//       } else {
+//         reject ('数字大于10即将执行失败回调')
+//       }
+//     }, 2000)
+//   })
+//   return p
+// }
+//
+// function promiseClickss () {
+//   let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       var num = Math.ceil(Math.random() * 20)
+//       console.log('随机数', num)
+//       if (num <= 10) {
+//         resolve(num)
+//       } else {
+//         reject ('数字大于10即将执行失败回调')
+//       }
+//     }, 2000)
+//   })
+//   return p
+// }
+//
+// promiseClick().then((data) => {
+//   console.log('resolved成功回调')
+//   console.log('成功回调接收的值：', data)
+//   console.log('hui')
+// }).catch((reason, data) => {
+//   console.log('catch到rejected失败回调')
+//   console.log('catch失败执行回调抛出失败原因:', reason)
+// })
 
 // all的用法
 // 与then同级的另一个方法，all方法，该方法提供了并行执行异步操作的能力，
 // 并且在所有异步操作执行完后并且执行结果都是成功的时候才执行回调
 
-Promise
-  .all([promiseClick(), promiseClicks(), promiseClickss()])
-  .then(function (results) {
-    console.log(results)
-  })
+// Promise
+//   .all([promiseClick(), promiseClicks(), promiseClickss()])
+//   .then(function (results) {
+//     console.log(results)
+//   })
 
 // race的用法
 // all是等所有的异步操作都执行完了再执行then方法，
