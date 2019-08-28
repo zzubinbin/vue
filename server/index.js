6028e02aed5cd5f41b24c2416249f830c7c30f96
huibin <iehuibinzzu@163.com>
2019-08-28 22:39:10 +0800

    study koa post

--- server/index.js
+++ server/index.js
@@ -27,4 +27,15 @@ const connectHandler = function connected() {
   eventEmitter.emit('data_received')
 }
 
-eventEmitter.on('o')
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', connectHandler);
+
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function(){
+  console.log('数据接收成功。');
+});
+
+// 触发 connection 事件
+eventEmitter.emit('connection');
+
+console.log("程序执行完毕。");
